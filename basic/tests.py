9dbea1af94bcd352a57c7b88cb05d5d90c834407
hugotuesta <hugoalexisz@hotmail.com>
2021-08-16 07:26:38 -0500

    Sum of two

--- basic/tests.py
+++ basic/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from contains_duplicates import contains_duplicates
+from sum_of_two import sum_of_two
 
 class TestContainsDuplicates(unittest.TestCase):
 
@@ -8,6 +9,16 @@ class TestContainsDuplicates(unittest.TestCase):
     
     def test_does_not_contains_duplicates(self):
         self.assertEqual(contains_duplicates([3, 1]), False)
+
+class TestSumOfTwo(unittest.TestCase):
+
+    def test_true_case(self):
+        test_result = sum_of_two([1, 2, 3], [10, 20, 30, 40], 42)
+        self.assertEqual(True, test_result)
+
+    def test_false_case(self):
+        test_result = sum_of_two([], [1, 2, 3, 4], 4)
+        self.assertEqual(False, test_result)
 
 if __name__ == '__main__':
     unittest.main()
