3bec66db6b93b02638637147787b01d5f7951e63
hugotuesta <hugoalexisz@hotmail.com>
2021-08-22 12:25:20 -0500

    Add unit tests for isListPalindrome

---
+++ linked_lists/is_list_palindrome.py
@@ -0,0 +1,15 @@
+# Singly-linked lists are already defined with this interface:
+class ListNode(object):
+  def __init__(self, x):
+    self.value = x
+    self.next = None
+
+def is_list_palindrome(l):
+    current = l 
+    elements = []
+
+    while current:
+        elements.append(current.value)
+        current = current.next
+    
+    return elements == elements[::-1]
