57e590544ce7b52f9ebed86e1d2255a91bd16113
hugotuesta <hugoalexisz@hotmail.com>
2021-08-22 11:43:51 -0500

    Add another version of removeKFromList

--- linked_lists/removeKfromList.py
+++ linked_lists/removeKfromList.py
@@ -4,26 +4,25 @@ class ListNode(object):
     self.value = x
     self.next = None
 
-def removeKFromList(l, k):  
-    previous = None
-    current = l
+def show_list(linked_list):
+  elements = []
+  current = linked_list
+  while current:
+    elements.append(current.value)
+    current = current.next
     
-    if current is None:
-        return l
-        
-    while current is not None:
-        if current.value == k and previous is None:
-            l = current.next
-            current = l
-        elif current.value == k and previous is not None:
-            next_item = current.next
-            previous.next = next_item
-            current = next_item
-        else:
-            previous = current
-            current = current.next  
+  return elements
 
-    return l
+def removeKFromList(l, k):  
+    current = l
+
+    while current:
+        if current.next and current.next.value == k:
+            current.next = current.next.next
+        else:
+            current = current.next
+
+    return l.next if l and l.value == k else l
 
 def remove_k_from_list(l, k):  
     dummy = ListNode(0)
@@ -41,3 +40,23 @@ def remove_k_from_list(l, k):
         l = l.next
     
     return dummy.next
+
+l1 = ListNode(3)
+l1.next = ListNode(1)
+l1.next.next = ListNode(2)
+l1.next.next.next = ListNode(3)
+l1.next.next.next.next = ListNode(4)
+l1.next.next.next.next.next = ListNode(5)
+
+ll1 = removeKFromList(l1, 3)
+print(show_list(ll1))
+
+l2 = ListNode(2)
+l2.next = ListNode(3)
+l2.next.next = ListNode(4)
+l2.next.next.next = ListNode(5)
+l2.next.next.next.next = ListNode(6)
+l2.next.next.next.next.next = ListNode(7)
+
+ll2 = removeKFromList(l2, 10)
+print(show_list(ll2))
