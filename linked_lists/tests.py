a9c0ba4d9adc75d3f0ff905005f992fabb8a7028
hugotuesta <hugoalexisz@hotmail.com>
2021-08-23 22:13:08 -0500

    Add tests to addTwoHugeNumbers

--- linked_lists/tests.py
+++ linked_lists/tests.py
@@ -2,6 +2,7 @@ import unittest
 from remove_k_from_list import show_list, removeKFromList, ListNode
 from is_list_palindrome import is_list_palindrome
 from merge_two_linked_lists import merge_two_linked_lists
+from add_two_huge_numbers import add_two_huge_numbers_with_linked_lists
 
 class TestRemoveKFromList(unittest.TestCase):
     
@@ -61,6 +62,34 @@ class TestMergeTwoLinkedLists(unittest.TestCase):
         merged_list = merge_two_linked_lists(l1=l1, l2=l2)
 
         self.assertEqual(show_list(merged_list), [0, 1, 1, 2, 3, 4, 5])
+
+class TestAddTwoHugeNumbers(unittest.TestCase):
+    
+    def test_add_two_number_first_case(self):
+        l1 = ListNode(9876)
+        l1.next = ListNode(5432)
+        l1.next.next = ListNode(1999)
+
+        l2 = ListNode(1)
+        l2.next = ListNode(8001)
+
+        result = add_two_huge_numbers_with_linked_lists(l1, l2)
+
+        self.assertEqual(show_list(result), [9876, 5434, 0])
+
+    def test_add_two_number_second_case(self):
+        l1 = ListNode(123)
+        l1.next = ListNode(4)
+        l1.next.next = ListNode(5)
+
+        l2 = ListNode(100)
+        l2.next = ListNode(100)
+        l2.next.next = ListNode(100)
+
+        result = add_two_huge_numbers_with_linked_lists(l1, l2)
+
+        self.assertEqual(show_list(result), [223, 104, 105])
+
 
 if __name__ == '__main__':
     unittest.main()
