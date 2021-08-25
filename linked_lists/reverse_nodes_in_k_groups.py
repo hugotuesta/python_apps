30ed933383a93b8cbd3d667f7eda25633b882af3
hugotuesta <hugoalexisz@hotmail.com>
2021-08-24 22:48:52 -0500

    Reverse nodes in K groups

---
+++ linked_lists/reverse_nodes_in_k_groups.py
@@ -0,0 +1,37 @@
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def reverse_nodes_in_k_groups(l, k):
+    dummy = ListNode(0)
+    dummy.next = l
+    groupPrev = dummy
+    
+    while True:
+        kth = get_kth(groupPrev, k)
+        if not kth:
+            break
+        groupNext = kth.next
+        
+        previous, current = kth.next, groupPrev.next
+        
+        while current != groupNext:
+            tmp = current.next
+            current.next = previous
+            previous = current
+            current = tmp
+        
+        tmp = groupPrev.next
+        groupPrev.next = kth
+        groupPrev = tmp
+        
+    return dummy.next
+    
+def get_kth(current, k):
+    while current and k > 0:
+        current = current.next
+        k -= 1
+    
+    return current
