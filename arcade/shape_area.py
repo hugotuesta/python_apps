9e0da30f12132bfc52f1afbd93221b274c592a49
hugotuesta <hugoalexisz@hotmail.com>
2021-08-30 08:06:15 -0500

    Shape Area and Make array consecutive

---
+++ arcade/shape_area.py
@@ -0,0 +1,10 @@
+def shape_area(n):
+    return 2 * n * (n - 1) + 1
+
+print(shape_area(n=1))
+print(shape_area(n=2))
+print(shape_area(n=3))
+print(shape_area(n=4))
+print(shape_area(n=5))
+print(shape_area(n=100))
+print(shape_area(n=9999))
