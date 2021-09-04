c82d1c61761180daee5f839a79df928761cefb7c
hugotuesta <hugoalexisz@hotmail.com>
2021-09-03 23:02:21 -0500

    Is lucky

---
+++ arcade/is_lucky.py
@@ -0,0 +1,10 @@
+def is_lucky(n):
+    string_n = str(n)
+    first_half = sum([int(i) for i in string_n[:len(string_n)//2]])
+    second_half = sum([int(i) for i in string_n[len(string_n)//2:]])
+   
+    return first_half == second_half
+
+print(is_lucky(1230))
+print(is_lucky(134008))
+print(is_lucky(261534))
