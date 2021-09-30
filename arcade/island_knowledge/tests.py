132457925b1a2ce80cddac45a6285c686ab150c7
hugotuesta <hugoalexisz@hotmail.com>
2021-09-29 20:47:56 -0500

    Is IPv4 address tests

--- arcade/island_knowledge/tests.py
+++ arcade/island_knowledge/tests.py
@@ -1,6 +1,7 @@
 import pytest
 from array_maximal_adjacent_difference import array_maximal_adjacent_difference
 from avoid_obstacles import avoid_obstacles
+from is_ipv4_address import is_ipv4_address
 
 def test_array_maximal_adjacent_difference_01():
     expected_result = array_maximal_adjacent_difference([2, 4, 1, 0])
@@ -41,5 +42,41 @@ def test_avoid_obstacles_03():
 def test_avoid_obstacles_04():
     expected_result = avoid_obstacles([19, 32, 11, 23])
     test_result = 3
+
+    assert test_result == expected_result
+
+def test_is_ipv4_address_01():
+    expected_result = is_ipv4_address('172.16.254.1')
+    test_result = True
+
+    assert test_result == expected_result
+
+def test_is_ipv4_address_02():
+    expected_result = is_ipv4_address('172.316.254.1')
+    test_result = False
+
+    assert test_result == expected_result
+
+def test_is_ipv4_address_03():
+    expected_result = is_ipv4_address('1.1.1.1a')
+    test_result = False
+
+    assert test_result == expected_result
+
+def test_is_ipv4_address_04():
+    expected_result = is_ipv4_address('.254.255.0')
+    test_result = False
+
+    assert test_result == expected_result
+
+def test_is_ipv4_address_05():
+    expected_result = is_ipv4_address('1.23.256..')
+    test_result = False
+
+    assert test_result == expected_result
+
+def test_is_ipv4_address_06():
+    expected_result = is_ipv4_address('0.254.255.0')
+    test_result = True
 
     assert test_result == expected_result
